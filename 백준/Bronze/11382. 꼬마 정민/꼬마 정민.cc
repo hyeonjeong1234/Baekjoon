9f682ca40f8a348a55bac4a6cbafa4d8bfe78a52
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-01-16 14:30:41 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+
+int main() {
+    long long a = 0;
+    long long b = 0;
+    long long c = 0;
+    long long sum = 0;
+    cin >> a>> b>> c;
+    sum = a + b + c;
+    cout << sum;
+    return 0;
+}
