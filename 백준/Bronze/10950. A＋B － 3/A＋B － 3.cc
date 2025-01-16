d77f58e8da91df7379a2e4bb9bce7dc16341a25a
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-01-16 14:33:42 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A;
+    int N;
+    int B;
+    cin>>N;
+    for(int i = 0; i<N;i++)
+    {
+        cin>>A>>B;
+        cout<<A+B<<endl;
+    }
+    return 0;
+}
