4d6b3ca0826afe5fd7ccddb85baeaee3e6d80be9
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-01-18 10:34:59 +0900

    [Silver III] Title: 주유소, Time: 48 ms, Memory: 3568 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -6,27 +6,26 @@ using namespace std;
 
 int main()
 {
-	int n,dis,val,min, result=0;
-	vector<int> dis_arr;
-	vector<int> val_arr;
+	unsigned int n,dis,val,min, result=0;
+	vector<unsigned int> dis_arr;
+	vector<unsigned int> val_arr;
 	cin >> n;
-	for (int i = 0; i < n-1; i++)
+	for (unsigned int i = 0; i < n-1; i++)
 	{
 		cin>>dis;
 		dis_arr.push_back(dis);
 	}
-	for (int i = 0; i < n; i++)
+	for (unsigned int i = 0; i < n; i++)
 	{
 		cin>>val;
 		val_arr.push_back(val);
 	}
-	for (int i = 0; i < n; i++)//지금노드를 최소값으로 생각하고 앞에 더 가격이싼 노드가 있으면 최소를 바꿈
+	min = val_arr[0];
+	for (unsigned int i = 0; i < n; i++)//0번노드를 최소값으로 생각하고 앞에 더 가격이싼 노드가 있으면 최소를 바꿈
 	{
-		min = val_arr[i];
-		for (int j = i; j >= 0; j--)
+		if (min > val_arr[i])
 		{
-			if (min > val_arr[j])
-				min = val_arr[j];
+			min = val_arr[i];
 		}
 		if (i != n - 1)
 		{
