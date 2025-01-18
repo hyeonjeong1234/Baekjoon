5b9be48e3dbaba94119de6f04211acff7a6cd5ee
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-01-18 10:28:33 +0900

    [Silver III] Title: 주유소, Time: 0 ms, Memory: 2020 KB, Score: 41 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	int n,dis,val, result=0;
+	int n,dis,val,min, result=0;
 	vector<int> dis_arr;
 	vector<int> val_arr;
 	cin >> n;
@@ -20,12 +20,19 @@ int main()
 		cin>>val;
 		val_arr.push_back(val);
 	}
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)//지금노드를 최소값으로 생각하고 앞에 더 가격이싼 노드가 있으면 최소를 바꿈
 	{
-		if (i != n-1)
+		min = val_arr[i];
+		for (int j = i; j >= 0; j--)
 		{
-			result += val_arr[i] * dis_arr[i];
+			if (min > val_arr[j])
+				min = val_arr[j];
 		}
+		if (i != n - 1)
+		{
+			result += min * dis_arr[i];
+		}
+		
 	}
 	cout << result;
 	return 0;
