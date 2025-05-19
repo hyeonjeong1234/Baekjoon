d5270db0e919c7218c7197395f50b593bbfe78f5
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-05-19 23:14:08 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -15,20 +15,15 @@ int main()
    
     cin >> N;
     
-    while (N >= i)
-    {
-
-        dp[i] = dp[i - 1] + 1;
-        if (i % 2 == 0)
-        {
-            dp[i] = min(dp[i], dp[i / 2] + 1);
-        }
-        if (i % 3 == 0) {
-            dp[i] = min(dp[i], dp[i / 3] + 1);
-        }
-       
+    while(i <= N)
+{
+  dp[i] = dp[i-1]+1;
+  if(i%2 == 0)
+    dp[i] = min(dp[i], dp[i/2]+1);
+  if(i%3 == 0)
+    dp[i] = min(dp[i], dp[i/3]+1);
         i++;
-    }
+}
     cout << dp[N];
     return 0;
 }
