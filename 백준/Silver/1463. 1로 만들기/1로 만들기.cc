6e8c82fe419459609f5e5ca6d68e77e1ac270082
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-05-19 22:29:07 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,34 @@
+//DP로 풀이
+// 1~8까지 손으로 계산해보면
+// 2,3으로 나눠지지 않는 경우 N을 1로 만드는 횟수 = N-1을 1로 만드는 횟수 +1
+// 2,3으로 나누어지는경우 N-1을 1로 만든경우에 +1 , 2로 나누는 경우,3,으로 나누는 경우 중 가장 작은거 선택
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int dp[1000001];
+int main()
+{
+    int N;
+    int i = 2;
+   
+    cin >> N;
+    
+    while (N >= i)
+    {
+
+        dp[i] = dp[i - 1] + 1;
+        if (i % 2 == 0)
+        {
+            dp[i] = min(dp[i], dp[i / 2] + 1);
+        }
+        if (i % 3 == 0) {
+            dp[i] = min(dp[i], dp[i / 3] + 1);
+        }
+       
+        i++;
+    }
+    cout << dp[N];
+    return 0;
+}
