05ac8efd2b59f640ec7f1032bfb531d1994942f6
hyeonjeong1234 <147581183+hyeonjeong1234@users.noreply.github.com>
2025-05-27 22:13:44 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -5,12 +5,16 @@ using namespace std;
 
 int main()
 {
-    int L, r = 31, M = 1234567891, H = 0;
+    int L, M = 1234567891;
+    unsigned long long H = 0, A = 0;
+    unsigned long long r = 1;
     string S;
     cin >> L >> S;
     for (int i = 0; i < L; i++)
     {
-        H += (S[i]-96) * pow(r, i);
+        H += (S[i] - 96) * r;
+        r = (r*31)%M;
     }
-    cout << H % M;
+    cout << H%M;
+
 }
